65a03a95e46b2faa2df5c335d9de9a2e75a3814c
Erik van Sebille <erikvansebille@gmail.com>
2025-11-17 21:28:54 +0100

    Update video-demo.js

--- src/components/video-demo.js
+++ src/components/video-demo.js
@@ -7,7 +7,7 @@ export const VideoDemo = () => {
     <Box id={'video-demo'} as='section' my={12}>
       <Container maxW='container.lg' centerContent>
         <Heading as='h1' size='2xl' mb={4}>
-          Getting started with VirtualShip
+          {/* HEADING TOEVOEGEN? */}
         </Heading>
         <Box
           w='100%'
@@ -19,9 +19,8 @@ export const VideoDemo = () => {
           <iframe
             width='100%'
             height='450'
-            src='https://www.youtube.com/embed/ypafzoTBj_A?si=gjQBAxzfNzslN4B_'
-            title='VirtualShip Classroom Demonstration'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            src='https://player.vimeo.com/external/1133141624.m3u8?s=a41df2d90cbb06b09fb6d6584260f4b6399c3fe7&oauth2_token_id=58974900'
+            title='Kinder Klimaat Concert Dag pitch'
             allowFullScreen
           ></iframe>
         </Box>
