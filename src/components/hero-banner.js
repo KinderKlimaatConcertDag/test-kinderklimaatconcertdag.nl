567e4582fdee70882559536d2b735a728a2a1f14
Erik van Sebille <erikvansebille@gmail.com>
2026-03-14 09:13:25 +0100

    Shortening hero banner text

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -23,16 +23,16 @@ export const HeroBanner = () => {
         </Stack>
         <Text py={5}>
           <Text as='span' textStyle={'paragraphBlock'}>
-            De Kinder Klimaat Concert Dag is een nationale dag waarop het perspectief van kinderen en jongeren op klimaatverandering centraal staat. De kracht van kinderen is dat ze een open blik hebben en eerlijke vragen stellen. Juist door hun kwetsbaarheid kunnen ze mensen aan het denken zetten.
+            Tijdens de Kinder Klimaat Concert Dag staat het perspectief van kinderen en jongeren op klimaatverandering centraal.
           </Text>
           <Text as='span' textStyle={'paragraphBlock'}>
-            De kracht die kinderen hebben als ze zich uiten in muziek en kunst is uniek. De Kinder Klimaat Concert Dag geeft 1 dag in het jaar kinderen en jongeren een platform om op hun manier op te komen voor hun toekomst, door muziek te maken, te tekenen, te dansen en te dichten.
-          </Text>
-          <Text as='span' textStyle={'paragraphBlock'} fontStyle='italic'>
-            Positief en hoopvol.
+            Kinderen hebben nog een open blik, stellen eerlijke vragen en kunnen door hun kwetsbaarheid mensen aan het denken zetten.
           </Text>
           <Text as='span' textStyle={'paragraphBlock'}>
-            Voor kinderen gaat het klimaat nog niet over voor- en tegenstanders van beleid. De Stichting Kinder Klimaat Concert Dag is volledig onafhankelijk en niet verbonden met politieke partijen. Wel werken we samen met culturele partners.
+            Daarom willen we kinderen één dag in het jaar een platform geven om door muziek te maken, te tekenen, te dansen en te dichten op te komen voor hun toekomst:{' '}
+            <Text as='span' display='inline' fontStyle='italic'>
+              positief en hoopvol!
+            </Text>
           </Text>
         </Text>
         <Box mt={8} w='100%'>
